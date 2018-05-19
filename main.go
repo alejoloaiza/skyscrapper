52cba63e98076bc4bf419b1aaa9dfc1808ee2096
alejoloaiza <alejo.loaiza@gmail.com>
2018-05-19 10:59:49 -0500

    [Add] Stopped here, waiting for a PR to be approved colly to continue

--- main.go
+++ main.go
@@ -9,21 +9,51 @@ import (
 )
 
 func main() {
-	const Url = "https://www.skyscanner.net/transport/flights-from/mdea/?adults=1&children=0&adultsv2=1&childrenv2=&infants=0&cabinclass=economy&rtn=1&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false&oym=1808&iym=1808&ref=home"
+	const Url = "https://www.skyscanner.net/transport/flights/mdea/syda/180801?adults=1&children=0&adultsv2=1&childrenv2=&infants=0&cabinclass=economy&rtn=0&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false#results"
 
 	fmt.Println("Collect start at: " + time.Now().Format(time.Stamp))
 	cLinks := colly.NewCollector(
 		colly.AllowedDomains("www.skyscanner.net"),
-	)
-	cDetails := colly.NewCollector(
-		colly.AllowedDomains("www.skyscanner.net"),
-		colly.Async(true),
 	)
 	cLinks.Limit(&colly.LimitRule{
 		DomainGlob:  "*www.skyscanner.net*",
 		Delay:       4 * time.Second,
 		RandomDelay: 2 * time.Second,
 	})
+
+	cLinks.OnHTML("a.CTASection__price-2bc7h.price", func(e *colly.HTMLElement) {
+		fmt.Println("Price found: " + e.Text)
+		/*link := e.Attr("href")
+		if strings.HasPrefix(link, "/webs/santafe/inmueble") {
+			cDetails.Visit(e.Request.AbsoluteURL(link))
+		} else if strings.HasPrefix(link, "/webs/santafe/pages/basico") {
+			cLinks.Visit(e.Request.AbsoluteURL(link))
+		} else {
+			return
+		}*/
+	})
+	cLinks.OnResponse(func(r *colly.Response) {
+		resp := string(r.Body)
+		target := "United States"
+		if strings.Contains(resp, target) {
+			fmt.Println("Yes found " + target)
+		}
+		fmt.Println(string(r.Body))
+	})
+	cLinks.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+	cLinks.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+		fmt.Println("RETRYING")
+		cLinks.Visit(r.Request.URL.String())
+	})
+	cLinks.Visit(Url)
+
+	cDetails := colly.NewCollector(
+		colly.AllowedDomains("www.skyscanner.net"),
+		colly.Async(true),
+	)
 	cDetails.Limit(&colly.LimitRule{
 		DomainGlob:  "*www.skyscanner.net*",
 		Parallelism: 5,
@@ -43,35 +73,5 @@ func main() {
 			fmt.Println("a")
 		}
 	})
-
-	// On every a element which has href attribute call callback
-	cLinks.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		/*link := e.Attr("href")
-		if strings.HasPrefix(link, "/webs/santafe/inmueble") {
-			cDetails.Visit(e.Request.AbsoluteURL(link))
-		} else if strings.HasPrefix(link, "/webs/santafe/pages/basico") {
-			cLinks.Visit(e.Request.AbsoluteURL(link))
-		} else {
-			return
-		}*/
-	})
-	cLinks.OnResponse(func(r *colly.Response) {
-		resp := string(r.Body)
-		target := "United States"
-		if strings.Contains(resp, target) {
-			fmt.Println("Yes found " + target)
-		}
-		//fmt.Println(string(r.Body))
-	})
-	cLinks.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-	cLinks.OnError(func(r *colly.Response, err error) {
-		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
-		fmt.Println("RETRYING")
-		cLinks.Visit(r.Request.URL.String())
-	})
-	cLinks.Visit(Url)
-
 	fmt.Println("Collect end at: " + time.Now().Format(time.Stamp))
 }
